6ce0dbec12407e62c2523f7dacbc0f5059c60500
Hung Nguyen <nthungdev@gmail.com>
2024-02-09 16:04:32 -0500

    fix matcher

--- prj1/prj1-sol/index.mjs
+++ prj1/prj1-sol/index.mjs
@@ -9,26 +9,25 @@ const scan = (str = '') => {
   const tokens = []
   let m
   for (let s = str; s.length > 0; s = s.slice(m[0].length)) {
-    if ((m = s.match(/^[ \t]+|^#.*$/))) {
-      // s starts with linear whitespace or comment
+    if ((m = s.match(/^[\s\t]+/))) {
+      // s starts with linear whitespace
+      continue
+    } else if ((m = s.match(/^#.*$/m))) {
+      // skip comment
       continue
     } else if ((m = s.match(/^true|false/))) {
-      // boolean
       tokens.push(new Token('boolean', m[0]))
     } else if ((m = s.match(/^\d+(_\d+)*/))) {
       // integer (with internal underscore)
       tokens.push(new Token('integer', m[0]))
-    } else if ((m = s.match(/^:[_a-zA-Z]+[_a-zA-Z0-9]*/))) {
-      // atom
+    } else if ((m = s.match(/^:[a-zA-Z]+[_a-zA-Z0-9]*/))) {
       tokens.push(new Token('atom', m[0]))
-    } else if ((m = s.match(/^%?{|^[\[\]}]|^=>\s*/))) {
-      // map or tuple bracket, =>
-      // TODO handle when not found closing bracket
+    } else if ((m = s.match(/^[a-zA-Z]+[_a-zA-Z0-9]*:/))) {
+      tokens.push(new Token('key', m[0]))
+    } else if ((m = s.match(/^%?{|^[\[\]}]|^=>*|,/))) {
+      // map or tuple bracket, =>, comma
+      // TODO handle error when not found closing bracket
       tokens.push(new Token('symbol', m[0]))
-    } else if ((m = s.match(/^./))) {
-      // TODO do I need this?
-      // any single char
-      tokens.push(new Token(m[0], m[0]))
     }
   }
   return tokens
@@ -38,10 +37,26 @@ class Token {
   constructor(kind, lexeme) {
     Object.assign(this, { kind, lexeme })
   }
+  toString() {
+    return `Token { kind: ${this.name}, lexeme: ${this.age} }`
+  }
 }
 
 const main = () => {
-  const [, , input] = process.argv
+  const tests = {
+    1: '%{a: 22, [33] => 44, c: {55, :d, []}}',
+    2: `# this file contains no data
+
+    #but it contains multiple comments`,
+    3: `# single atom
+
+    :atom
+
+    `,
+  }
+
+  // const [, , input] = process.argv
+  const input = tests[3]
   console.log('input', input)
   console.log('tokens', scan(input))
 }
